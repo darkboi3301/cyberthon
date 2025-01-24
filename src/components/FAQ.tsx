474645b47b245497c834e0b0a3e90ea380b07863
Yogavelan-BJ <yogavelanbj@gmail.com>
2025-01-24 13:36:52 +0530

    changes

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -13,7 +13,7 @@ const FAQ = () => {
     },
     {
       question: "What is the registration fee for Cyberthon?",
-      answer: "The registration fee for Cyberthon is Rs.1000",
+      answer: "The registration fee for Cyberthon is Rs.1500",
     },
     {
       question: "What's the duration of cyberthon?",
